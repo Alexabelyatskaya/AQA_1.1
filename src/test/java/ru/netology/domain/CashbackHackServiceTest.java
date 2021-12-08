caa7adc9dba6cc2094216f331b6239e0354245fe
Alexandra <alexabelyatskaya@yandex.ru>
2021-12-08 10:56:37 +0300

    Задание 1.1.1.TNG

--- src/test/java/ru/netology/domain/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/domain/CashbackHackServiceTest.java
@@ -1,34 +1,34 @@
 package ru.netology.domain;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldService0() {
-        assertEquals(1000, service.remain(0));
+        assertEquals(service.remain(0), 1000);
     }
 
     @Test
     public void shouldServiceUnder1000() {
-        assertEquals(590, service.remain(410));
+        assertEquals(service.remain(410), 590);
     }
 
     @Test
     public void shouldService1000() {
-        assertEquals(0, service.remain(1000));
+        assertEquals(service.remain(1000), 0);
     }
 
     @Test
     public void shouldServiceMore1000() {
-        assertEquals(880, service.remain(1120));
+        assertEquals(service.remain(1120), 880);
     }
 
     @Test
     public void shouldServiceMore10000() {
-        assertEquals(680, service.remain(10320));
+        assertEquals(service.remain(10320), 680);
     }
 }
